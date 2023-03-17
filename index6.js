4b4bd7d9544d0ad5e267276350ac77a3c77858a3
Archik-K <ar.kryatova@yandex.ru>
2023-03-17 22:53:25 +0300

    dz2

---
+++ index6.js
@@ -0,0 +1,7 @@
+/*У вас есть две переменные:
+    const greetings = "Привет";
+    const name = "Кот";
+    Склейте значение этих двух переменных и выведите на экран значение «Привет, Кот». */
+const greetings = "Привет,";
+const name = "Кот";
+console.log(greetings + " " + name);
