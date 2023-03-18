bc6466e2bd7906e25b12ba0771d3e5acd42462fe
Archik-K <ar.kryatova@yandex.ru>
2023-03-18 22:40:43 +0300

    dz3

--- index3.js
+++ index3.js
@@ -1,6 +1,14 @@
 /*    Прокрастинируя дома, Русалка 128 минут переписывалась в мессенджере, 
       254 минуты лайкала фотокарточки, 137 минут читала статьи ВКонтакте и 201 минуту смотрела стендапы на YouTube.
       Выведите в консоль, сколько часов прокрастинировала Русалка. */
-let procastinationMermaid =
-  128 + 254 + 137 + 201; /* складываем все значения. */
-console.log(procastinationMermaid);
+const correspondenceMermaid = 128;
+const likephotoMermaid = 254;
+const readarticlesMermaid = 137;
+const watchedyoutubeMermaid = 201;
+/* складываем все значения. */
+console.log(
+  correspondenceMermaid +
+    likephotoMermaid +
+    readarticlesMermaid +
+    watchedyoutubeMermaid
+);
